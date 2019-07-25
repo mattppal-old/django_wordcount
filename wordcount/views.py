f6ca8baa350d0015fcb623aaba717ed3596564ce
Matt <mattppalmer@gmail.com>
2019-07-25 12:39:29 -0400

    init

---
+++ wordcount/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from collections import defaultdict
+import operator
+
+def home(request):
+  return render(request, 'home.html',)
+
+def count(request):
+  fulltext = request.GET['fulltext']
+  wordlist = fulltext.split()
+  count_dict = defaultdict(int)
+  for word in wordlist:
+    count_dict[word] += 1
+  sorted_words = sorted(count_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+  return render(request, 'count.html', {'fulltext':fulltext,'count':len(wordlist),'count_dict':sorted_words})
+
+def about(request):
+  return render(request, 'about.html')
+
+def test(request):
+  return render(request, 'test.html')
